01f8759d24a640b1b2e27d70976b202b4f53cd2d
abhishek <abhishekrajkumar1999@gmail.com>
2025-07-15 17:24:05 +0530

    initial commit: setting up structure

---
+++ src/main/java/com/example/splitWiser/SplitWiserApplication.java
@@ -0,0 +1,13 @@
+package com.example.splitWiser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWiserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitWiserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/splitWiser/SplitWiserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.splitWiser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitWiserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
